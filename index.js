21cb2cd1f13f60101ca8398eb79ebab653fdc3e6
Steve Godin <steve.god@gmail.com>
2017-05-05 15:54:40 -0700

    Add ability to post questions

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ function getQuestion (id) {
     .then(function (res) { return res.json() });
 }
 
+function postQuestion (questionParams) {
+  return fetch(
+    `${DOMAIN}/api/v1/questions?api_token=${API_TOKEN}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // {title: 'dasdas', body: 'dasdas'}
+      // should look like👇
+      // {question: {title: 'dasdadssa', body: 'dasdas'}}
+      body: JSON.stringify({question: questionParams})
+    }
+  )
+}
+
 function renderQuestions (questions) {
   return questions.map(function (question) {
     return `
@@ -28,28 +44,45 @@ function renderQuestions (questions) {
         </a>
       </div>
     `
-  }).join('')
+  }).join('');
 }
 
 function renderQuestion (question) {
+  console.log(question);
   return `
     <button class="back">Back</button>
     <h1>${question.title}</h1>
     <p>${question.body}</p>
-  `
+    <h4>Answers</h4>
+    <ul class="answers-list">
+      ${renderAnswers(question.answers)}
+    </ul>
+  `;
 }
 
+function renderAnswers (answers) {
+  return answers.map(function (answer) {
+    return `<li class="answer">${answer.body}</li>`;
+  }).join('');
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
-  // We put out DOM queries inside a DOMContentLoaded event handler
+  // We put our DOM queries inside a DOMContentLoaded event handler
   // because the queried nodes at likely not rendered yet.
   // JavaScript inside of a DOMContentLoaded event handler will
   // run once every HTML tag has been rendered by the browser
   const questionsList = document.querySelector('#questions-list');
   const questionDetails = document.querySelector('#question-details');
+  const questionForm = document.querySelector('#question-form');
 
-  getQuestions()
+  function loadQuestions () {
+    getQuestions()
     .then(renderQuestions)
     .then(function (html) { questionsList.innerHTML = html });
+  }
+
+  loadQuestions();
 
   questionsList.addEventListener('click', function (event) {
     const { target } = event;
@@ -74,6 +107,22 @@ document.addEventListener('DOMContentLoaded', function () {
       questionDetails.classList.add('hidden');
       questionsList.classList.remove('hidden');
     }
+  });
+
+  questionForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const title = event.currentTarget.querySelector('#title');
+    const body = event.currentTarget.querySelector('#body');
+
+    const fData = new FormData(event.currentTarget);
+
+    postQuestion({title: fData.get('title'), body: fData.get('body')})
+      .then(function () {
+        loadQuestions();
+        title.value = '';
+        body.value = '';
+      })
   })
 });
 
