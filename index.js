28a20038aa84c5046e42980824ee8a7eac6603d8
Steve Godin <steve.god@gmail.com>
2017-05-05 14:22:37 -0700

    Render questions list on page load

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const API_TOKEN = '1A4OLp2eq97LmrSBECYYpGateFMY6BLVeow70rz0ktM';
 // and returns an array of questions in a promise
 function getQuestions () {
   return fetch(`${DOMAIN}/api/v1/questions?api_token=${API_TOKEN}`)
-    .then(function (res) { return res.json() })
+    .then(function (res) { return res.json() });
 }
 
 // getQuestion makes request to our Rails API backend
@@ -13,5 +13,28 @@ function getQuestions () {
 // the given id
 function getQuestion (id) {
   return fetch(`${DOMAIN}/api/v1/questions/${id}?api_token=${API_TOKEN}`)
-    .then(function (res) { return res.json() })
+    .then(function (res) { return res.json() });
 }
+
+function renderQuestions (questions) {
+  return questions.map(function (question) {
+    return `
+      <div class="question-summary">
+        ${question.title}
+      </div>
+      <hr />
+    `
+  }).join('')
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // We put out DOM queries inside a DOMContentLoaded event handler
+  // because the queried nodes at likely not rendered yet.
+  // JavaScript inside of a DOMContentLoaded event handler will
+  // run once every HTML tag has been rendered by the browser
+  const questionsList = document.querySelector('#questions-list');
+
+  getQuestions()
+    .then(renderQuestions)
+    .then(function (html) { questionsList.innerHTML = html })
+});
